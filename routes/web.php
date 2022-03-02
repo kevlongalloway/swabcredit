c6bc0627e75201b013fc4d82ab3c545a8c7c54e1
Kevlon Galloway <kevlongalloway@Kevlons-MacBook-Pro.local>
2022-03-02 09:34:27 -0500

    delete unused files

--- routes/web.php
+++ routes/web.php
@@ -28,18 +28,9 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::get('/credit-restoration', [CRController::class, 'index'])->name('cr.index');
-Route::post('/credit-restoration', [CRController::class, 'store'])->name('cr.store');
-
-Route::get('tax-preparation', [TPController::class, 'index'])->name('tp.index');
-
-Route::get('business-formation', [BFController::class, 'index'])->name('bf.index');
-
-Route::post('/payment', [PaymentController::class, 'index'])->name('payment.index');
-
-Route::get('/upload-documents', [UploadController::class, 'index'])->name('upload.index');
-
 Route::get('/services/{servicePath}', [ServicesController::class, 'index'])->name('guest.services')->middleware(['auth.subscribe','service.exists']);
 Route::post('/checkout', [ServicesController::class, 'store'])->name('to.checkout');
+
+Route::get('/upload-documents', [UploadController::class, 'index'])->name('upload.index');
 
 require_once __DIR__ . '/jetstream.php';
