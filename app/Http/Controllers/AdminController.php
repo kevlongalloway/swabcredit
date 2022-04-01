34047620bc37402b127b8ff328cb6c03e9496e7d
kevlongalloway <kevlongalloway1999m@gmail.com>
2022-03-31 20:09:50 -0400

    upload functionality

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,16 @@ use App\Models\User;
 
 class AdminController extends Controller
 {
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        
+    }
+
     public function dashboard()
     {
         return view('dashboard', ['customers' => User::all()]);
--- app/Http/Livewire/UploadForm.php
+++ app/Http/Livewire/UploadForm.php
@@ -9,27 +9,132 @@ class UploadForm extends Component
 {
     use WithFileUploads;
 
+    /**
+     * The current step on the upload component.
+     *
+     * @var int
+     */
     public $currentStep;
+
+    /**
+     * The total amount of steps.
+     *
+     * @var int
+     */
     public $totalSteps = 3;
+
+    /**
+     * If upload form started.
+     *
+     * @var bool
+     */
     public $start;
+
+    /**
+     * The message to the user.
+     *
+     * @var string
+     */
     public $message = '';
 
+    /**
+     * User input for account number.
+     *
+     * @var string
+     */
     public $acc_num;
+
+    /**
+     * User input for routing number.
+     *
+     * @var string
+     */
     public $rout_num;
+
+    /**
+     * User input for filing status.
+     *
+     * @var int
+     */
     public $filing_status;
+
+    /**
+     * User input for carrier
+     *
+     * @var int
+     */
     public $carrier;
+
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
+    public $idf;
+
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
+    public $idb;
+
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
+    public $w2;
+
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
+    public $tax_g;
+
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
+    public $utility_bill;
+
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
+    public $snn;
+
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
+    public $tax_k;
+
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
+    public $etc;
+
+    /**
+     * Input Attributes  that are file uploads.
+     *
+     * @var array<string>
+     */
     public $fileAttributes = [
         'idf','idb','w2', 'tax_g','utility_bill','snn','tax_k','etc'
     ];
-    public $idf;
-    public $idb;
-    public $w2;
-    public $tax_g;
-    public $utility_bill;
-    public $snn;
-    public $tax_k;
-    public $etc;
 
+    /**
+     * User input for file ID Front.
+     *
+     * @var array<string>
+     */
     protected $validationAttributes = [
         'idf' => 'ID (Front)',
         'idb' => 'ID (Back)',
@@ -41,41 +146,76 @@ class UploadForm extends Component
         'etc' => 'Report Card, Progress Report, or Medical Record'
     ];
 
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
     public function mount()
     {
         $this->currentStep = 1;
         $this->start = false;
     }
 
+    /**
+     * Display the success modal with a message.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function render()
     {
         return view('livewire.upload-form');
     }
 
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
     public function start()
     {
         $this->start = true;
     }
 
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
     public function next()
     {
         $this->currentStep++;
     }
+
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
     public function back()
     {
         $this->currentStep--;
     }
 
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
     public function save()
     {
         $this->validateFiles();   
 
-        if(env('APP_ENV' == 'production')) {
-            $this->updateUser();
-        }
+        $this->updateUser();
+        
         $this->message = 'Successfully uploaded!!';
     }
 
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
     public function finish()
     {
         $this->validate([
@@ -92,13 +232,17 @@ class UploadForm extends Component
 
         $this->validateFiles();
 
-        if(env('APP_ENV' == 'production')) {
-            $this->updateUser();
-        }
+        $this->updateUser();
+        
         $this->message = 'Successfully uploaded!';
         return redirect()->route('success', ['type' => 'uploaded']);
     }
 
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
     protected function updateUser()
     {
         $user = auth()->user();
@@ -107,17 +251,22 @@ class UploadForm extends Component
             'rout_num' => $this->rout_num,
             'filing_status' => $this->filing_status,
             'carrier' => $this->carrier,
-            'id_front_filename' => $this->idf->store('service/'.$user->name),
-            'id_back_filename' => $this->idb->store('service/'.$user->name),
-            'w2_filename' => $this->w2->store('service/'.$user->name),
-            'tax_g_filename' => $this->tax_g->store('service/'.$user->name),
-            'utility_bill_filename' => $this->utility_bill->store('service/'.$user->name),
-            'snn_filename' => $this->snn->store('service/'.$user->name),
-            'tax_k_filename' => $this->tax_k->store('service/'.$user->name),
-            'etc_filename' => $this->etc->store('service/'.$user->name)
+            'id_front_filename' => $this->idf->store('docs'),
+            'id_back_filename' => $this->idb->store('docs'),
+            'w2_filename' => $this->w2->store('docs'),
+            'tax_g_filename' => $this->tax_g->store('docs'),
+            'utility_bill_filename' => $this->utility_bill->store('docs'),
+            'snn_filename' => $this->snn->store('docs'),
+            'tax_k_filename' => $this->tax_k->store('docs'),
+            'etc_filename' => $this->etc->store('docs')
         ]);
     }
 
+    /**
+     * User input for file ID Front.
+     *
+     * @var int
+     */
     protected function validateFiles()
     {
         foreach($this->fileAttributes as $attribute) {
